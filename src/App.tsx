aab7c4d77c83f9dd50b16fabdf41ed4fe487f16e
letniki <nikitaletuta12@gmai.com>
2024-08-03 12:46:45 +0300

    create lesson 13 redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React, {FC} from 'react';
 import './App.css';
+import {decrement, increment, incrementByAmount, useAppDispatch, useAppSelector} from "./redux/store";
+import {useDispatch} from "react-redux";
 
 const App: FC = ()=> {
+    const value=useAppSelector(state => {
+        return state.counter1SliceState.value
+    });
+    const dispatch = useAppDispatch();
   return (
-    <>
-    </>
+    <div>
+        <h2>{value}</h2>
+      <button onClick={()=>{
+         dispatch(increment());
+      }}> increment</button>
+        <button onClick={() => {
+            dispatch(decrement())
+        }}> decrement</button>
+        <button onClick={()=>{
+            dispatch(incrementByAmount(100))
+        }}>increment by 100</button>
+    </div>
   );
 }
 
